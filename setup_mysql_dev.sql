0d22a4ec1bd78facdf7a1c2c5623ddbb9a46d39a
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-16 23:22:49 +0000

    added .*

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- MySQL setup developement
+-- MySQL setup dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
