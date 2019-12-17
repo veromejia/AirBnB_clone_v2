c36cf40f654f87ac010f82085bf0fa885d9f4062
veromejia <veromejia.q@gmail.com>
2019-12-17 15:16:40 -0800

    user update

--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
 
 
 class User(BaseModel):
@@ -11,7 +17,8 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
