cf44866a67b836d0f614f2b7127b1d534c8a21a5
veromejia <veromejia.q@gmail.com>
2019-12-17 15:55:47 -0800

    update review

--- models/review.py
+++ models/review.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This is the review class"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
 
 
 class Review(BaseModel):
@@ -10,6 +16,8 @@ class Review(BaseModel):
         user_id: user id
         text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
