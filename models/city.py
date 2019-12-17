d7c2f4e04eb67e51932ef6fab5e7aedb8d04f0d4
veromejia <veromejia.q@gmail.com>
2019-12-17 14:49:46 -0800

    update city file

--- models/city.py
+++ models/city.py
@@ -3,11 +3,16 @@
 from models.base_model import BaseModel
 
 
+Base = declarative_base()
+
+
 class City(BaseModel):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(string(60), nullable=False, ForeignKey('states.id'))
+    
