faa3ab8d62ca55e12b79c51e4408fe5cdc9124df
veromejia <veromejia.q@gmail.com>
2019-12-18 10:41:03 -0800

    importing base

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
