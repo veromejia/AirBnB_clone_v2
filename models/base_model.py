34b1d95b916d4364fd4ffa4c9b385059d600061f
veromejia <veromejia.q@gmail.com>
2019-12-17 21:28:59 -0800

    adding delete method

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,8 @@
 import uuid
 import models
 from datetime import datetime
-
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -13,10 +14,9 @@ class BaseModel:
     for other classes
     """
 
-    id = Column((string(60), nullable=False, primary_key=True)
-    create_at = Column(DataTime, nullable=False, default=datetime.utcnow())
-    update_at = Column(DataTime, nullable=False, default=datetime.utcnow())0
-
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -34,6 +34,13 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            time_now = datetime.now()
+            if "created_at" not in kwargs:
+                self.created_at = time_now
+            if "updated_at" not in kwargs:
+                self.updated_at = time_now
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -67,4 +74,11 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict.keys():
+            my_dict.pop('_sa_instance_state', None)
         return my_dict
+
+    def delete(self):
+        '''Deletes the current instance from storage by calling method
+        delete'''
+        models.storage.delete(self)
