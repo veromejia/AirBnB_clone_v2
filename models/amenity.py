4c449d52d326a7912859dbbd9d896c18688df530
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-19 23:36:10 +0000

    amenity relationship

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -12,4 +13,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = 
+    place_amenities = relationship("Place",
+                                    secondary=place_amenity)
