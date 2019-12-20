c61c218dbd02af9b1f9dba979079a4dcaea3d581
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-20 00:25:58 +0000

    Place relationship with amenity

--- models/place.py
+++ models/place.py
@@ -12,7 +12,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
-                             ForeignKey('amenity_id'),
+                             ForeignKey('amenities.id'),
                              primary_key=True, nullable=False))
 
 
