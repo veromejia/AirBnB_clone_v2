42bb95842dfa33e8ee3681a876c31a02d66208e4
veromejia <veromejia.q@gmail.com>
2020-01-03 14:04:55 -0800

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from the contents of the web_static folder"""
+
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    """packs files into .tgz file"""
+    tgz_file = 'versions/web_static_{}.tgz'\
+        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
+    local("mkdir -p versions")
+    new_file= local("tar -cvzf {} web_static".format(tgz_file))
+
+    if new_file.succeeded:
+        return tgz_file
+    return None
