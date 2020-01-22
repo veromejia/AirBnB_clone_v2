394e9771543c33396e777de9b244a5ac357bbaaa
veromejia <veromejia.q@gmail.com>
2020-01-22 00:30:46 -0800

    def reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
