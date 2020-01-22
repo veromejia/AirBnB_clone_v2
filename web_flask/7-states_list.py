ef134038287778389df18643e89df002ad324a37
veromejia <veromejia.q@gmail.com>
2020-01-21 22:51:03 -0800

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page: inside the tag BODY"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
